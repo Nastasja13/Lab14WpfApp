e4ce6675be7ffcf056696fdec1ab352347c45ec7
Anastacia Golovaneva <nastasja13.com@gmail.com>
2022-03-08 00:59:47 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF14
+{   
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                PrName = "Яблоки",
+                PrPrice = 89,
+                PrImage = "Data/Яблоко.jpg",
+                PrCategory = PrCategorys.Food
+            });
+            products.Add(new Product()
+            { 
+                PrName = "Мандарины",
+                PrPrice = 140,
+                PrImage = "Data/Мандарины.jpg",
+                PrCategory = PrCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                PrName = "Виноград",
+                PrPrice = 229,
+                PrImage = "Data/Виноград.jpg",
+                PrCategory = PrCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                PrName = "Фен",
+                PrPrice = 2538,
+                PrImage = "Data/Фен.jpg",
+                PrCategory = PrCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                PrName = "Мультиварка",
+                PrPrice = 10900,
+                PrImage = "Data/Мультиварка.jpg",
+                PrCategory = PrCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                PrName = "Увлажнитель воздуха",
+                PrPrice = 24998,
+                PrImage = "Data/Воздухоувлажнитель.jpg",
+                PrCategory = PrCategorys.Appliances
+            });
+            listBox.ItemsSource = products; 
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF14
+{    
+    public enum PrCategorys
+    {
+        Food,
+        Appliances
+    }
+    
+    public class Product
+    {
+        public string PrName{ get; set; }
+        public decimal PrPrice{ get; set; }
+        public string PrImage{ get; set; }
+        public PrCategorys PrCategory{ get; set; }
+
+        public override string ToString()
+        {
+            return PrName;
+        }
+    }
+
+
+}
